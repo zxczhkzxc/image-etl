f0476523a64f1ae576ee8a5a0e6c39ff0b2f0470
zhanghongke <zhanghongke@dianwoda.com>
2019-04-30 16:22:46 +0800

    update

--- object_detection_etl/class_num_count.py
+++ object_detection_etl/class_num_count.py
@@ -134,7 +134,7 @@ def parse_args(args):
     csv_parser.add_argument('annotations=', help='Path to CSV file containing annotations for evaluation.')
     csv_parser.add_argument('classes=', help='Path to a CSV file containing class label mapping.')
     # parser.add_argument('--pascal_path', help='Path to dataset directory (ie. /tmp/VOCdevkit).')
-    parser.add_argument('model',              help='Path to RetinaNet model.')
+    parser.add_argument('--model',              help='Path to RetinaNet model.')
     parser.add_argument('--convert-model',    help='Convert the model to an inference model (ie. the input is a training model).', action='store_true')
     parser.add_argument('--backbone',         help='The backbone of the model.', default='resnet50')
     parser.add_argument('--gpu',              help='Id of the GPU to use (as reported by nvidia-smi).')
